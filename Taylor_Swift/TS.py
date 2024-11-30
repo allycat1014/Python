ae6fe20d460d3639872b7aeb5c77d61789068e0d
ananya <ananyakotadia@gmail.com>
2024-11-29 22:42:14 -0800

    trends between popularity and danceability

--- Taylor_Swift/TS.py
+++ Taylor_Swift/TS.py
@@ -8,15 +8,13 @@ taylor_file = pandas.read_csv('tay_data/taylor_swift_spotify.csv', encoding='lat
 taylors = taylor_file.to_dict(orient='records')
 
 '''
-
-Find the longest song across all albums
-Find the album with highest average danceability across all her albums
 find trend between popularity and danceabliltiy
-given a favorite song give a list of recommendations 
 '''
 songs = {}
 albums = {}
 list1 = []
+danceability_list = []
+popularity_list = []
 
 class Similarity:
     def __init__(self, name, score):
@@ -56,25 +54,38 @@ for song in taylors:
     else:
         song_obj = songs[name]
     album_obj = None
-
-
-'''
-Identify the most popular song in each album.   
     if album_name not in albums.keys():
         album_obj = Album(album_name)
         albums[album_name] = album_obj
     else:
         album_obj = albums.get(album_name)
     album_obj.tracks.add(song_obj)
-    if album_obj.most_popular_track is None:
-        album_obj.most_popular_track = song_obj
-    elif song_obj.popularity > album_obj.most_popular_track.popularity:
-        album_obj.most_popular_track = song_obj
-
+for album in albums.values():
+    sum = 0
+    for track in album.tracks:
+        sum =+ track.danceability
+    album.average_danceability = sum/len(album.tracks)
+for album in albums.values():
+    sum = 0
+    for track in album.tracks:
+        sum += track.popularity
+    album.average_popularity = sum/len(album.tracks)
 
 for album in albums.values():
-    print(album.name, "  :: ",  album.most_popular_track.name)
-'''
+    popularity_list.append(album)
+    danceability_list.append(album)
+
+def sortByDanceability(album):
+    return album.average_danceability
+
+def sortByPopularity(album):
+    return album.average_popularity
+
+danceability_list.sort(key=sortByDanceability)
+popularity_list.sort(key=sortByPopularity)
+print(danceability_list)
+print(popularity_list)
+
 
 '''
 given a favorite song give a list of recommendations 
@@ -91,6 +102,46 @@ def getScore(similarity):
 list1.sort(key=getScore)
 print(list1)
 '''
+'''
+Find the album with highest average danceability across all the albums
+highest_average_danceability = 0
+highest_average_danceability_album = ''
+for album in albums.values():
+    sum = 0
+    for track in album.tracks:
+        sum =+ track.danceability
+    album.average_danceability = sum/len(album.tracks)
+    if highest_average_danceability < album.average_danceability:
+        highest_average_danceability = album.average_danceability
+        highest_average_danceability_album = album.name
+
+
+print(highest_average_danceability_album, "  :: ",  highest_average_danceability)
+'''
+'''
+Find the longest song across all albums
+   
+    if album_obj.longest_song is None:
+        album_obj.longest_song = song_obj
+    elif song_obj.duration > album_obj.longest_song.duration:
+        album_obj.longest_song = song_obj
+
+for album in albums.values():
+    print(album.name, "  :: ", album.longest_song.name)
+'''
+'''
+Identify the most popular song in each album.   
+    if album_obj.most_popular_track is None:
+        album_obj.most_popular_track = song_obj
+    elif song_obj.popularity > album_obj.most_popular_track.popularity:
+        album_obj.most_popular_track = song_obj
+
+
+for album in albums.values():
+    print(album.name, "  :: ",  album.most_popular_track.name)
+'''
+
+
 '''
 def calculate_similarity(song1, song2):
     features = ['danceability', 'energy', 'acousticness', 'valence', 'tempo']
