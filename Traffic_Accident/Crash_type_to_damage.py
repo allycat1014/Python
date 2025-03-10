3d255ea7c989c705a89dafc85edb842da5ad903a
ananya <ananyakotadia@gmail.com>
2025-03-09 20:14:41 -0700

    crash_type_to_damage class

--- Traffic_Accident/Crash_type_to_damage.py
+++ Traffic_Accident/Crash_type_to_damage.py
@@ -1,7 +1,7 @@
 class Crash_type_to_damage:
-    def __init__(self, crash_type):
-        self.crash_type = crash_type
+    def __init__(self, first_crash_type):
+        self.first_crash_type = first_crash_type
         self.total_damage = 0
         def __repr__(self):
-            return repr((self.crash_type))
+            return repr((self.first_crash_type))
 
